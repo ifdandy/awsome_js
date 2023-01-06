6219ff2c575defdbea20b79abbe81c8a2b2ba68b
ifdandy <vapsvr@qq.com>
2023-01-06 22:01:24 +0800

    Create seo_auto_push.js

---
+++ seo_auto_push.js
@@ -0,0 +1,25 @@
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+(function(){
+var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
+(function(){
+var el = document.createElement("script");
+el.src = "https://sf1-scmcdn-tos.pstatp.com/goofy/ttzz/push.js?d9e55c4fdd4e5133ca15a61aea2eed8366400d87a35e0af2f1a2b58df59ac772932b0e7039e2d26f282318ad3ff64a5277708db420e28cf4c4dfa77352ac31212b8d7c8c6655c9b00211740aa8a98e2e";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window);
+</script>
