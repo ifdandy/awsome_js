8e570810abb6c2df9c11887c7fba4c0a9f26bdb9
ifdandy <vapsvr@qq.com>
2023-01-29 13:15:57 +0800

    完整版

--- bd_share.js
+++ bd_share.js
@@ -1,31 +1,28 @@
-<script>
-  # 这里可以随机推送同域名的的URL
-	(function() {
-		var canonicalURL, curProtocol;
-		var x = document.getElementsByTagName("link");
-		if (x.length > 0) {
-			for (i = 0; i < x.length; i++) {
-				if (x[i].rel.toLowerCase() == 'canonical' && x[i].href) {
-					canonicalURL = x[i].href
-				}
-			}
-		}
-		if (!canonicalURL) {
-			curProtocol = window.location.protocol.split(':')[0]
-		} else {
-			curProtocol = canonicalURL.split(':')[0]
-		}
-		if (!canonicalURL) canonicalURL = window.location.href; !
-		function() {
-			var e = /([http|https]:/ / [a - zA - Z0 - 9_.] + .baidu.com) / gi,
-			r = canonicalURL,
-			t = document.referrer;
-			if (!e.test(r)) {
-				var n = (String(curProtocol).toLowerCase() === 'https') ? "https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif": "//api.share.baidu.com/s.gif";
-				t ? (n += "?r=" + encodeURIComponent(document.referrer), r && (n += "&l=" + r)) : r && (n += "?l=" + r);
-				var i = new Image;
-				i.src = n
-			}
-		} (window)
-	})();
-</script>
+(function() {
+    window.task = $('dd a');
+    window.share_num = 0;
+    window.share_timer = null;
+    setTimeout(function () {
+        bd_share(false)
+    }, 1500);
+    window.share_timer = setInterval(function() {
+        if(window.share_num > 5){
+            clearInterval(window.share_timer);
+            return true;
+        }
+        bd_share($(window.task[window.share_num]).attr('href'))
+        window.share_num ++;
+    },4500);
+    function bd_share(share_url) {
+        curProtocol = window.location.href.split(':')[0]
+        var n = (String(curProtocol).toLowerCase() === 'https') ? "https://sp0.baidu.com/9_Q4simg2RQJ8t7jm9iCKT-xh_/s.gif": "//api.share.baidu.com/s.gif";
+        if(share_url){
+            n += "?r=" + encodeURIComponent(window.location.href), share_url && (n += "&l=" + share_url)
+        }else{
+            t = document.referrer;
+            t ? (n += "?r=" + encodeURIComponent(document.referrer), n += "&l=" + window.location.href) : n += "?l=" + window.location.href;
+        }
+        var i = new Image;
+        i.src = n
+    }
+})();
